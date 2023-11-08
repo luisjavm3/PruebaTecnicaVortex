dfc1bf28261f2b111a4c9fe1f319dee11cffa79d
Luis J. Montes <luisjavm3@gmail.com>
2023-11-08 14:42:13 -0500

    Initial Commit

---
+++ src/main/java/PruebaTecnicaVortex/domain/entities/Conductor.java
@@ -0,0 +1,10 @@
+package PruebaTecnicaVortex.domain.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class Conductor {
+
+}
---
+++ src/test/java/PruebaTecnicaVortex/PruebaTecnicaVortexApplicationTests.java
@@ -0,0 +1,13 @@
+package PruebaTecnicaVortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaVortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
